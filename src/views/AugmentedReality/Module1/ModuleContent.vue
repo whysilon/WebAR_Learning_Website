272a9f02ce612fe0754b66ed8c79b1e1a9686ded
whysilon <popoxdha@gmail.com>
2024-12-10 17:36:10 +0800

    Added more content to introduction

--- src/views/AugmentedReality/Module1/ModuleContent.vue
+++ src/views/AugmentedReality/Module1/ModuleContent.vue
@@ -1,4 +1,28 @@
+<script setup>
+import { Card, Image } from 'primevue';
+</script>
+
 <template>
-    <h1> This is the first module of Augmented Reality</h1>
-    <p> This is the content of this module</p>
+    <h1> Introduction to Augmented Reality </h1>
+    <p> The most famous application of Augmented Reality (AR) is probably Pokemon Go, where you could catch the pokemon in "real life"</p>
+    <div style="text-align: center;">
+        <Image src="/src/assets/picture.png" alt="Pokemon Go"/>
+        <p> Gameplay of Pokemon Go </p>
+    </div>
+    <h2> Definition of AR </h2>
+    <p> What is Augmented Reality? As the term suggests, it involves enhancing the physical environment in some way.
+        < AR Definition >
+        It involves three main characteristics [1]</p>
+    <Card>
+        <template #title>
+            Characteristics of AR
+        </template>
+        <template #content>
+                <ul>
+                    <li>Real and virtual objects exists simultaneously </li>
+                    <li>The environment can be interacted with in real time</li>
+                    <li>Real and virtual objects interact and respond together to changes in the environment</li>
+                </ul>
+        </template> 
+    </Card>
 </template>
