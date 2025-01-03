2fc93a85a68b5e17ae57ede75d7f73c2b46928a8
whysilon <popoxdha@gmail.com>
2025-01-03 11:08:34 +0800

    Added abit of content to CV intro

--- src/views/ComputerVision/IntroToComputerVision/IntroToComputerVisionContent.vue
+++ src/views/ComputerVision/IntroToComputerVision/IntroToComputerVisionContent.vue
@@ -1,3 +1,12 @@
 <template>
-    <h1>Placeholder content</h1>
+    <h1>Introduction</h1>
+    <p> Have you ever wondered how our eyes perceive the text you are currently reading? How are we
+        able to "see" what we are seeing? And the last question is, how do cameras or computers even
+        take pictures or videos and convert them into somethign that they can understand?
+    </p>
+    <p>
+        In this module, we will be introducing the ideas of computer vision and how AR uses it to 
+        create the magic that we see in our phones and other devices. We will be discussing the different
+        methods which computers use to perceive and distinguish the objects and environment around them.
+    </p>
 </template>
