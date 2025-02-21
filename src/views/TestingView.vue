c4a70a46e06b156356a4ad31b342342cca3f1a67
whysilon <valencino1306@gmail.com>
2025-02-21 14:13:43 +0800

    Removed Babylon, Added MindAR

--- src/views/TestingView.vue
+++ src/views/TestingView.vue
@@ -1,23 +1,8 @@
 <template>
   <div>
-    <canvas ref="bjsCanvas" width="500" height="500" />
+    <p>Hi</p>
   </div>
 </template>
 
 <script>
-import { createScene } from "../scenes/MyFirstScene";
-
-export default {
-  name: "BabylonScene",
-
-  mounted() {
-    const bjsCanvas = this.$refs.bjsCanvas;
-    if (bjsCanvas) {
-      const fpsCallback = (fps) => {
-        this.$emit("fps", fps);
-      };
-      createScene(bjsCanvas, fpsCallback);
-    }
-  },
-};
 </script>
