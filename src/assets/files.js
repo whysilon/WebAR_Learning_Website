8f4b9712d62ecd8455014cfe2f2c8d3537a08fad
whysilon <valencino1306@gmail.com>
2025-03-13 18:41:09 +0800

    Updated all local files into links

---
+++ src/assets/files.js
@@ -0,0 +1,12 @@
+export const NTU_COAT_OF_ARMS = "https://v6puk9518l.ufs.sh/f/UrpUiAQE237V4FQIvjg1AlW3nwPDjB7sQcKRz86ZVyYartIE";
+export const NTU_LOGO = "https://v6puk9518l.ufs.sh/f/UrpUiAQE237VHZANQg5dCiYczTl9kaFXx8J0nehZDAuvSfwK";
+export const NTU_COAT_SOBEL = "https://v6puk9518l.ufs.sh/f/UrpUiAQE237VT4lPX5xqUYAVy3om1h4PqfOrdECaDipcI56K";
+export const TILES = "https://v6puk9518l.ufs.sh/f/UrpUiAQE237VgHCyVa2ZHz0OFLJGYmDXpM5Wjd9b4SoPqyA3";
+export const NTU_COAT_GRAYSCALE = "https://v6puk9518l.ufs.sh/f/UrpUiAQE237VJ6taEEvGCzGwMaJuhSgNKYHk83eTW62LB10x";
+export const POKEMON_GAMEPLAY = "https://v6puk9518l.ufs.sh/f/UrpUiAQE237VQDrbuuU86dohfkTuC1HIUteYB5yG3XRApa0v";
+export const NTU_LOGO_MIND = "https://v6puk9518l.ufs.sh/f/UrpUiAQE237VGAVmbreTzFIg3KsJoEiXBrvQ2akP01yqpD8l";
+export const HIGH_CONTRAST = "https://v6puk9518l.ufs.sh/f/UrpUiAQE237VHaohfC5dCiYczTl9kaFXx8J0nehZDAuvSfwK";
+export const LOW_CONTRAST = "https://v6puk9518l.ufs.sh/f/UrpUiAQE237V5JJwz4Pc65Edm2kBC9uGcxRflOrt0iY8L1XD";
+export const LOW_CONTRAST_FEATURE_MAP = "https://v6puk9518l.ufs.sh/f/UrpUiAQE237VPGMxV59WD5TJBF3a9SViZNgbp7YvyeRfxULM";
+export const HIRO_MARKER_ARJS = "https://v6puk9518l.ufs.sh/f/UrpUiAQE237V6acGXgBfCU0YH5qTXr2D6hcWKAZwJ4ISP7OM";
+export const HIGH_CONTRAST_FEATURE_MAP = "https://v6puk9518l.ufs.sh/f/UrpUiAQE237Vb8LQ1iHqhEYWXGDsy41vtKil3T9H5aNzU0pr";
